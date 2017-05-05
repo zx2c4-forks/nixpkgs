085593e34a009c544e57286a6016899c754f137e
Robin Gloster <mail@glob.in>
2017-05-05 18:30:44 +0200

    aircrack-ng: add unreleased patch for openssl 1.1

--- pkgs/tools/networking/aircrack-ng/default.nix
+++ pkgs/tools/networking/aircrack-ng/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl, libpcap, openssl, zlib, wirelesstools
-, iw, ethtool, pciutils, libnl, pkgconfig, makeWrapper }:
+, iw, ethtool, pciutils, libnl, pkgconfig, makeWrapper, fetchpatch
+}:
 
 stdenv.mkDerivation rec {
   name = "aircrack-ng-1.2-rc4";
@@ -12,7 +13,16 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkgconfig makeWrapper ];
   buildInputs = [ libpcap openssl zlib libnl iw ethtool pciutils ];
 
-  patchPhase = ''
+  patches = [
+    (fetchpatch {
+      url = "https://trac.aircrack-ng.org/changeset/2882?format=diff&new=2882";
+      name = "openssl-1.1.patch";
+      sha256 = "11vk89jyd42l13i9y8l2p92p0cf4cdza40qpq75avvb7zqh3c2yi";
+      stripLen = 2;
+    })
+  ];
+
+  postPatch = ''
     sed -e 's@^prefix.*@prefix = '$out@ -i common.mak
     sed -e 's@/usr/local/bin@'${wirelesstools}@ -i src/osdep/linux.c
   '';
