4b453c0e82fdd020dd20f3aacaf68a594cd2169a
Robin Gloster <mail@glob.in>
2017-08-03 19:13:49 +0200

    cyrus_sasl: 2.1.26 -> 2.1.27rc3
    build with openssl 1.1

--- pkgs/development/libraries/cyrus-sasl/default.nix
+++ pkgs/development/libraries/cyrus-sasl/default.nix
@@ -4,11 +4,11 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "cyrus-sasl-${version}${optionalString (kerberos == null) "-without-kerberos"}";
-  version = "2.1.26";
+  version = "2.1.27rc3";
 
   src = fetchurl {
     url = "ftp://ftp.cyrusimap.org/cyrus-sasl/${name}.tar.gz";
-    sha256 = "1hvvbcsg21nlncbgs0cgn3iwlnb3vannzwsp6rwvnn9ba4v53g4g";
+    sha256 = "0aivwvkacfwhblqfl8xj006633dw85pxplgdy6wa64yk5822j3x5";
   };
 
   outputs = [ "bin" "dev" "out" "man" "devdoc" ];
