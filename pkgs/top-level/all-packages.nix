f37a48278d5cb3571cfae98ad75b1e288ba6981e
Robin Gloster <mail@glob.in>
2017-08-11 16:35:17 +0200

    python27: don't pin to openssl_1_0_2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6992,7 +6992,6 @@ with pkgs;
   python27 = callPackage ../development/interpreters/python/cpython/2.7 {
     self = python27;
     inherit (darwin) CF configd;
-    openssl = openssl_1_0_2;
   };
   python34 = callPackage ../development/interpreters/python/cpython/3.4 {
     inherit (darwin) CF configd;
